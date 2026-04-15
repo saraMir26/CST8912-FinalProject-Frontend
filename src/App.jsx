fef93dc726a33bfa1ba42a863652f8b042a9bbf5
saraMir26 <mirz0014@algonquinlive.com>
2026-04-15 18:20:57 -0400

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ChatPage from "./pages/ChatPage";
